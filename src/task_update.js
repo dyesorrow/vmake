690c77fa1229b4aab8c9db5b2390b87381dca765
hxm <dyesorrow@qq.com>
2022-06-02 12:42:43 +0000

    修改更新

--- src/task_update.js
+++ src/task_update.js
@@ -2,5 +2,6 @@ const path = require('path')
 
 vmake.task.update = function(){
     vmake.run("git pull", path.dirname(__dirname));
+    vmake.run("npm config set package-lock false", path.dirname(__dirname));
     vmake.run("npm install", path.dirname(__dirname));
 }
